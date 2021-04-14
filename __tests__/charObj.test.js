4b4ff4cfdc6713baef6a272027f38f9174fcb1bc
James Wyn <thisisjameswyn@gmail.com>
2021-04-14 11:09:58 -0700

    second test fail
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>

--- __tests__/charObj.test.js
+++ __tests__/charObj.test.js
@@ -3,7 +3,14 @@ import Character from '../src/js/charObj.js';
 describe('Character', () => {
 
   test('should instantiate a new basic char object', () => {
-    let myChar = new Character()
+    let myChar = new Character();
     expect(myChar).toBeDefined();
   });
+  test('should instaniate character key and values', () => {
+    let myChar = new Character("warrior");
+    expect(myChar).toEqual("warrior");
+  })
 });
+
+//let warStats = [10, 4];
+
